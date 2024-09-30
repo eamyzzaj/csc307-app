e790c0c9adc326a876100efaab075b7dab9374e5
jmgdls <jazzymae.delossantos@gmail.com>
2024-09-29 19:57:00 -0700

    Implement IA1:step3 and IA1:step4

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -2,12 +2,47 @@
 import React from "react";
 import Table from "./Table";
 
+const characters = [
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ];
+  
+
 function MyApp() {
+
     return (
-      <div className="container">
-        <Table />
-      </div>
-    );
+        <div className="container">
+          <Table characterData={characters} />
+        </div>
+      );
+
+    
   }
+
+  
+
+  /*
+  function characterData() {
+    return (
+        <div className="container">
+          <Table characterData={characters} />
+        </div>
+      );
+  }
+  */
 
 export default MyApp;
