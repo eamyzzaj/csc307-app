d0401058c67ce1750aa90255dc9e96f1d0c36886
jmgdls <jazzymae.delossantos@gmail.com>
2024-09-29 20:24:32 -0700

    Implement IA1:step 5

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -5,6 +5,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -13,18 +14,25 @@ function TableHeader() {
   function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
-          <tr key={index}>
+            <tr key={index}>
             <td>{row.name}</td>
             <td>{row.job}</td>
-          </tr>
+            <td>
+                <button onClick={() => props.removeCharacter(index)}>
+                 Delete
+                </button>
+            </td>
+        </tr>
         );
        }
       );
+      
       return (
           <tbody>
             {rows}
            </tbody>
        );
+       
   }
 
 
@@ -32,7 +40,10 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody 
+         characterData={props.characterData}
+         removeCharacter={props.removeCharacter}
+        />
       </table>
     );
 }
