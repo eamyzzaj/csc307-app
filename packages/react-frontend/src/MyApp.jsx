14f43e2ce1d013b004d96db4e4377c7a30c8ab1a
jmgdls <jazzymae.delossantos@gmail.com>
2024-09-29 20:41:09 -0700

    Implement IA1:step 7

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,7 +1,9 @@
 // src/MyApp.jsx
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from "./Form";
 
+/*
 const characters = [
     {
       name: "Charlie",
@@ -20,28 +22,11 @@ const characters = [
       job: "Bartender"
     }
   ];
-  
+*/
 
 function MyApp() {
 
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor"
-        },
-        {
-          name: "Mac",
-          job: "Bouncer"
-        },
-        {
-          name: "Dee",
-          job: "Aspring actress"
-        },
-        {
-          name: "Dennis",
-          job: "Bartender"
-        }
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -53,10 +38,12 @@ function MyApp() {
 
     return (
         <div className="container">
-          <Table 
-           characterData={characters}
-           removeCharacter={removeOneCharacter} 
-         />
+            <Table 
+             characterData={characters}
+             removeCharacter={removeOneCharacter} 
+            />
+            <Form />
+         
         </div>
       );
 
