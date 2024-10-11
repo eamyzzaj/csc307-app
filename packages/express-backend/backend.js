bf55f19e4b41410365f4f9491eed42490ecda89f
jmgdls <jazzymae.delossantos@gmail.com>
2024-10-10 18:17:00 -0700

    Finished implementing IA2:step 7 all the way through

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,6 +33,28 @@ const findUserByName = (name) => {
     }
   });
 
+  const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name && user["job"] === job);
+  };
+
+  app.get("/users", (req, res) => {
+      const name = req.query.name;
+      const job = req.query.job;
+      console.log("Working");
+      if (name && job) {
+        const result = findUserByNameAndJob(name, job);
+        if (result.length > 0) {
+          res.status(200).json(result);
+        } else {
+          res.status(404).json({error: "No user found"});
+        }
+      }
+      else {
+        res.status(400).json({error: "Please provide both name and job"});
+      }
+  });
+
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
   
