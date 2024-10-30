fb571bd3e0b74aa82e08794ea564c37e994751da
jmgdls <jazzymae.delossantos@gmail.com>
2024-10-29 18:56:24 -0700

    Implement unit tests for sum and containsNumbers functions

--- packages/jest-unit-testing/module.test.js
+++ packages/jest-unit-testing/module.test.js
@@ -1,8 +1,37 @@
 // module.test.js
 import mut from './module.js'; // MUT = Module Under Test
 
+// sum FUNCTION TESTING
 test('Testing sum -- success', () => {
   const expected = 30;
   const got = mut.sum(12, 18);
+  expect(got).toBe(expected);
+});
+
+// div FUNCTION TESTING
+
+// containsNumbers TESTING 
+test('Testing containsNumbers -- success', () => {
+  const expected = true;
+  const got = mut.containsNumbers('123');
+  expect(got).toBe(expected);
+});
+
+test('Testing containsNumbers -- failure', () => {
+  const expected = false;
+  const got = mut.containsNumbers('abc');
+  expect(got).toBe(expected);
+});
+
+test('Testing containsNumbers -- failure (empty text)', () => {
+  const expected = false;
+  const got = mut.containsNumbers('');
+  expect(got).toBe(expected);
+});
+
+// BUG -> javascript counts 'space' as a 0
+test('Testing containsNumbers -- failure (empty text with spaces)', () => {
+  const expected = false;
+  const got = mut.containsNumbers('  ');
   expect(got).toBe(expected);
 });
